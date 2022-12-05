73a6fcc0a04af0558f221a090c292799e2de0087
FernandaSanchezR <2794747S@student.gla.ac.uk>
2022-12-05 14:49:47 +0000

    Second commit

---
+++ R/FernandaSanchezRSeries03-package.R
@@ -0,0 +1,22 @@
+#' FernandaSanchezRSeries03
+#'
+#' \code{FernandaSanchezRSeries03} is a dummy package containing a simple birth-death
+#' difference equation model.
+#'
+#' @author
+#' Sonia Mitchell <sonia.mitchell@glasgow.ac.uk> (maintainer) \cr
+#' Richard Reeve <richard.reeve@glasgow.ac.uk>
+#'
+#' @name FernandaSanchezRSeries03-package
+#' @aliases FernandaSanchezRSeries03
+#' @docType package
+#'
+#' @import RPiR
+#'
+#' @examples
+#' \dontrun{
+#' # To run the demo
+#' demo("d0105_run_birth_death", package = "FernandaSanchezRSeries03")
+#' }
+#'
+NULL
